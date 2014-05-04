f8725acfb5d8dae9661fbd388934bb425493f05f
Max Irwin <max_irwin@yahoo.com>
2014-05-03 23:09:52 -0400

    Proof of concept

--- parser.js
+++ parser.js
@@ -19,7 +19,9 @@ var restlang = (function() {
 	};
 
 	//Splits a line into tokens
-	var tokenize = function(text) {return text.split(/[\s]+/); };
+	var tokenize = function(text) {
+		return text.substr(1).split(/[\s:]+/);
+	};
 
 	//Parses the source
 	var parse = function parse(source) {
@@ -27,7 +29,6 @@ var restlang = (function() {
 		var api = [],
 			char = null,
 			stack = [],
-			current = null,
 			tokens = [],
 			lines = [],
 			line = '',
@@ -50,84 +51,72 @@ var restlang = (function() {
 		//Pops the stack until it reaches an object of the specified type
 		var popto = function(type,errormessage){
 			while(stack.length && stack[0].type!==type) stack.shift();
-			if(!stack.length) error(errormessage);			
+			if  (!stack.length) error(errormessage);			
 			return stack[0].obj;
 		};
 
 		//Adds a new API resource
 		var resource = function(tokens) {
-			var name = tokens[0].substr(1);
-			var obj = {name:name};
+			var name = tokens.shift().toLowerCase();
+			var obj = {name:name,description:tokens.join(' '),resource:{}};
+
 			api.push(obj);
 			stack = [{type:'resource',obj:obj}];
-			current = stack[0];
 		};
 
 		//Adds a new resource method
 		var method = function(tokens) {
 			var curr = popto('resource',"The method '"+tokens[0]+"' does not apply to a resource.");
-			var name = tokens[0].substr(1).toUpperCase();
-			var obj = curr[name];
-			if(!obj) obj = curr[name] = {};
+			var name = tokens.shift().toUpperCase();
+			var obj = curr.resource[name];
+			if(!obj) obj = curr.resource[name] = {};
+			if(tokens.length) obj.description = tokens.join(' ');
+
 			stack.unshift({type:'method',obj:obj});
 		};
 
-		//Adds a new resource method
-		var param = function(tokens) {
-			var curr = popto('method',"The route parameter '"+tokens[0]+"' does not apply to a method.");
-			var name = tokens[0].substr(1).toLowerCase();
-			curr.params = curr.params||{};
-			var obj = curr.params[name];
-			if(!obj) obj = curr.params[name] = {};
-			stack.unshift({type:'param',obj:obj});
-
-			obj.type = tokens[1];
+		//Adds an external command reference
+		var command = function(line) {
+			var curr = popto('method',"The command '"+line+"' does not apply to a method.");
+			var external = line.replace(/[{}]/g,'').split(':');
+			if (external.length!==2) error("The command format '"+line+"' was not recognised");
+			curr.command = {file:external[0],method:external[1]};
 		};
 
-		//Adds a new resource method
-		var query = function(tokens) {
-			var curr = popto('method',"The querystring parameter '"+tokens[0]+"' does not apply to a method.");
-			var name = tokens[0].substr(1).toLowerCase();
-			curr.params = curr.params||{};
-			var obj = curr.params[name];
-			if(!obj) obj = curr.params[name] = {};
-			stack.unshift({type:'query',obj:obj});
-		};
-
-		//Adds a new resource method
-		var body = function(tokens) {
-			var curr = popto('method',"The body parameter '"+tokens[0]+"' does not apply to a method.");
-			var name = tokens[0].substr(1).toLowerCase();
-			curr.params = curr.params||{};
-			var obj = curr.params[name];
-			if(!obj) obj = curr.params[name] = {};
-			stack.unshift({type:'body',obj:obj});
-		};
-
-		//Adds a new resource method
-		var file = function(tokens) {
-			var curr = popto('method',"The file attachment '"+tokens[0]+"' does not apply to a method.");
-			var name = tokens[0].substr(1).toLowerCase();
-			curr.params = curr.params||{};
-			var obj = curr.params[name];
-			if(!obj) obj = curr.params[name] = {};
-			stack.unshift({type:'file',obj:obj});
-		};
-
-		//Adds a new resource method
-		var field = function(tokens) {
-			var curr = popto('method',"The return field '"+tokens[0]+"' does not apply to a method.");
-			var name = tokens[0].substr(1).toLowerCase();
-			curr.params = curr.params||{};
-			var obj = curr.params[name];
-			if(!obj) obj = curr.params[name] = {};
-			stack.unshift({type:'field',obj:obj});
-		};
-
+		//Adds description text to current stack object
 		var description = function(line) {
 			var curr = stack[0].obj;
 			curr.description = curr.description ? (curr.description+' '+line) : line;
 		};
+
+		//Composes a function to add a type of method request or response parameter
+		var parameter = function(key,errormessage) {
+			//Adds a method parameter item
+			return function(tokens) {
+				if(tokens.length===1) error("A datatype is missing for '"+tokens[0]+"'");
+				var curr = popto('method',errormessage.replace('%s',tokens[0]));
+				var name = tokens.shift().toLowerCase();
+				curr[key] = curr[key]||{};
+				var obj = curr[key][name];
+				if(!obj) obj = curr[key][name] = {};
+				stack.unshift({type:key,obj:obj});
+				obj.type = tokens.shift();
+				if(tokens.length && tokens[0]==='required') {
+					obj.required=true;
+					tokens.shift();
+				}
+				if(tokens.length) obj.description = tokens.join(' ');
+			}
+		}
+
+		//Declare method request parameter functions
+		var param = parameter('params',"The route parameter '%s' does not apply to a method.");
+		var query = parameter('query',"The querystring parameter '%s' does not apply to a method.");
+		var body = parameter('body',"The body parameter '%s' does not apply to a method.");
+		var file = parameter('files',"The file attachment '%s' does not apply to a method.");
+
+		//Declate method response parameter functions
+		var response = parameter('response',"The response field '%s' does not apply to a method.");
 
 		//Loop through all the lines and parse the source
 		for(i=0,l=lines.length;i<l;i++) {
@@ -138,12 +127,13 @@ var restlang = (function() {
 
 			switch(char) {
 				case '/': resource(tokens); break;
-				case '@': method(tokens); break;
+				case '#': method(tokens); break;
 				case ':': param(tokens); break;
 				case '?': query(tokens); break;
-				case '-': body(tokens); break;
-				case '|': field(tokens); break;
+				case '@': body(tokens); break;
 				case '$': file(tokens); break;
+				case '{': command(line); break;
+				case '|': response(tokens); break;
 				default: description(line); break;
 			}
 
