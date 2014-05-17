471061bc79a36216fa2532cd0942c682974c327d
Max Irwin <max_irwin@yahoo.com>
2014-05-17 17:22:16 +0100

    New tokenizer

--- parser.js
+++ parser.js
@@ -33,6 +33,19 @@ var restlang = (function() {
 	var rxStringN = /string(\d)+/i;
 
 	// --------------------------------------------------------------
+	// SyntaxError Exception Object
+	var SyntaxError = function(message,at,text) {
+		this.name = 'SyntaxError';
+		this.at = at;
+		this.text = text;
+	};
+
+	SyntaxError.prototype.toString = function() {
+		return this.name + ' at line number ' + this.at + ': ' + this.text;
+	};
+
+
+	// --------------------------------------------------------------
 	// Trims whitespace
 	var trim = function(text) { 
 		if (!text||!text.length) return '';
@@ -46,14 +59,8 @@ var restlang = (function() {
 	};
 
 	// --------------------------------------------------------------
-	// Splits a line into tokens
-	var tokenize = function(text) {
-		return text.substr(1).split(/[\s:]+/);
-	};
-
-	// --------------------------------------------------------------
 	// Splits a line into a tokens object
-	var tokenize2 = function(line) {
+	var tokenize = function(line) {
 		var tokens = {};
 		var idx = 0;
 		var len = line.length||0;
@@ -73,11 +80,19 @@ var restlang = (function() {
 		var requirable = ['route','query','body','file'];
 		var typeable = ['route','query','body','response'];
 		var mutable = ['resource','method'];
+		var identity = ['identity','parent'];
+		var authentication = ['authentication']
 
 		var keywords = {
 			'required':requirable,
-			'mutable':mutable
+			'mutable':mutable,
 		};
+
+		var settings = {
+			'default':requirable,
+			'level':authentication
+		};
+			
 
 		if (len) {
 			//Get Symbol and line type
@@ -99,6 +114,8 @@ var restlang = (function() {
 		var datatype = '';
 		var token = '';
 		var keyword = null;
+		var setting = null;
+		var setable = null;
 		while (!done) {
 
 			//Next character in line
@@ -137,17 +154,28 @@ var restlang = (function() {
 					} else {
 						tokens.error = "The datatype '"+token+"' cannot apply to a " + tokens.type;
 					}
+				} else if (token.indexOf('=')>-1 && (setable=settings[setting=token.substr(0,token.indexOf('='))])) {
+					//Token is a setting
+					if(setable.indexOf(setting)) {
+						setting = token.split('=');
+						tokens[setting[0]] = setting[1];
+					} else {
+						tokens.error = "The setting '"+setting+"' cannot apply to a " + tokens.name;						
+					}
 				}
 
 				//Next token
 				token = '';
 				space = false;
+				setting = null;
+				setable = null;
 			}
 
 			if (done) {
 				//End of line!  Cleanup and return
 				if(named) {
-					tokens.description = line.substr(idx);
+					line = trim(line.substr(idx).replace(/^\s*:/,''));
+					tokens.description = line;
 				} else {
 					tokens = {type:'description',name:line};
 				}
@@ -155,27 +183,25 @@ var restlang = (function() {
 			}
 		}
 
-		//Spot the futility of this statement:
+		//Halting problem in-joke:
 		return tokens;
 
 	};
 
-	//Parses the source
+	// --------------------------------------------------------------
+	// Parses the source
 	var parse = function parse(source) {
 
 		var api = [],
-			char = null,
 			stack = [],
-			tokens = [],
+			tokens = {},
 			lines = [],
 			line = '',
 			i = 0,
 			l = 0;
 
 		//Throws a Syntax Error found while parsing
-		var error = function(message) {
-			throw { name:'SyntaxError', message:message, at:i, text:line };
-		};
+		var error = function(message) { throw new SyntaxError(message, i, line); };
 
 		source = trim(source);
 
@@ -203,29 +229,27 @@ var restlang = (function() {
 
 		//Adds a new API resource
 		var resource = function(tokens) {
-			var name = tokens.shift().toLowerCase();
-			var obj = {name:name,description:tokens.join(' '),resource:{}};
-
+			var obj = {name:tokens.name,resource:{}};
 			api.push(obj);
 			stack = [{type:'resource',obj:obj}];
 		};
 
 		//Adds a new resource method
 		var method = function(tokens) {
-			var curr = popto('resource',"The method '"+tokens[0]+"' does not apply to a resource.");
-			var name = tokens.shift().toUpperCase();
+			var curr = popto('resource',"The method '"+tokens.name+"' does not apply to a resource.");
+			var name = tokens.name;
 			var obj = curr.resource[name];
 			if(!obj) obj = curr.resource[name] = {};
-			if(tokens.length) obj.description = tokens.join(' ');
+			if(tokens.description) obj.description = tokens.description;
 
 			stack.unshift({type:'method',obj:obj});
 		};
 
 		//Identity property, acts as a 'Primary Key' of an entry
 		var identity = function(tokens){
-			var curr = popto(['method','resource'],"The command '"+line+"' does not apply to a method or resource.");
+			var curr = popto(['method','resource'],"The identity '"+tokens.name+"' does not apply to a method or resource.");
 			curr.identity = curr.identity||[];
-			var id = { name:tokens.shift().toLowerCase() };
+			var id = { name:tokens.name };
 			curr.identity.push(id);
 
 			if(tokens.length) id.description = tokens.join(' ');
@@ -236,7 +260,7 @@ var restlang = (function() {
 		//Parent property, acts as a 'Foreign key' of an entry
 		var parent = function(tokens) {
 			var curr = popto('method');
-			curr = curr||popto('resource',"The command '"+line+"' does not apply to a method or resource.");
+			curr = curr||popto('resource',"The parent '"+line+"' does not apply to a method or resource.");
 			if(tokens.length===0) error("A parent resource is missing for '"+line+"'");
 			if(tokens.length===1) error("A parent id is missing for '"+tokens[0]+"'");
 			curr.parent = curr.parent||[];
@@ -253,26 +277,24 @@ var restlang = (function() {
 		//Mutable property, marks that the entity state will change when method is called
 		var mutable = function(tokens) {
 			var curr = popto('method');
-			curr = curr||popto('resource',"The command '"+line+"' does not apply to a method or resource.");
+			curr = curr||popto('resource',"The mutable '"+line+"' does not apply to a method or resource.");
 			curr.mutable = {ismutable:true};
-			if(tokens.length) curr.mutable.description = tokens.join(' ');			
+			if(tokens.description) curr.mutable.description = tokens.description;
 			return curr.mutable;
 		};
 
 		//Authentication property, denotes security access level required for the method
 		var authentication = function(tokens){
 			var curr = popto('method');
-			curr = curr||popto('resource',"The command '"+line+"' does not apply to a method or resource.");
-			if(tokens.length===0) error("An authentication level is missing for '"+line+"'");
-			var level = tokens.shift();
-			curr.authentication = {level:level};
-			if(tokens.length) curr.authentication.description = tokens.join(' ');			
+			curr = curr||popto('resource',"The authentication '"+line+"' does not apply to a method or resource.");
+			curr.authentication = {level:tokens.level};
+			if(tokens.description) curr.authentication.description = tokens.description;
 			return curr.authentication;
 		};
 
 		//Adds a new method or resource property
 		var property = function(tokens) {
-			var keyword = tokens.shift().toLowerCase();
+			var keyword = tokens.name;
 			var obj = null;
 			switch (keyword) {
 				case 'identity': obj = identity(tokens); break;
@@ -286,41 +308,40 @@ var restlang = (function() {
 		}
 
 		//Adds an external command reference
-		var command = function(line) {
-			var curr = popto('method',"The command '"+line+"' does not apply to a method.");
-			var external = line.replace(/[{}]/g,'').split(':');
-			if (external.length!==2) error("The command format '"+line+"' was not recognised");
-			curr.command = {file:external[0],method:external[1]};
+		var command = function(tokens) {
+			var curr = popto('method',"The command '"+tokens.name+"' does not apply to a method.");
+			curr.command = trim(line).replace(/^\{/,'').replace(/\}$/,'');
 		};
 
 		//Adds description text to current stack object
-		var description = function(line) {
+		var description = function(tokens) {
 			var curr = stack[0].obj;
-			curr.description = curr.description ? (curr.description+' '+line) : line;
+			curr.description = curr.description ? (curr.description+' '+tokens.description) : tokens.description;
 		};
 
 		//Composes a function to add a type of method request or response parameter
 		var parameter = function(key,errormessage) {
 			//Adds a method parameter item
 			return function(tokens) {
-				if(tokens.length===1) error("A datatype is missing for '"+tokens[0]+"'");
-				var curr = popto('method',errormessage.replace('%s',tokens[0]));
-				var name = tokens.shift().toLowerCase();
+
+				if(!tokens.name) error("A name is missing for '"+line+"'");
+				if(!tokens.datatype) error("A datatype is missing for '"+tokens.name+"'");
+
+				var curr = popto('method',errormessage.replace('%s',tokens.name));
 				curr[key] = curr[key]||{};
+
+				var name = tokens.name;
 				var obj = curr[key][name];
 				if(!obj) obj = curr[key][name] = {};
 				stack.unshift({type:key,obj:obj});
-				obj.type = tokens.shift();
-				if(tokens.length && tokens[0]==='required') {
-					obj.required=true;
-					tokens.shift();
-				}
-				if(tokens.length) obj.description = tokens.join(' ');
+				obj.type = tokens.datatype;
+				if(tokens.require) obj.required = true;
+				if(tokens.description) obj.description = tokens.description;
 			}
 		}
 
 		//Declare method request parameter functions
-		var param = parameter('params',"The route parameter '%s' does not apply to a method.");
+		var route = parameter('params',"The route parameter '%s' does not apply to a method.");
 		var query = parameter('query',"The querystring parameter '%s' does not apply to a method.");
 		var body = parameter('body',"The body parameter '%s' does not apply to a method.");
 		var file = parameter('files',"The file attachment '%s' does not apply to a method.");
@@ -330,24 +351,22 @@ var restlang = (function() {
 
 		//Loop through all the lines and parse the source
 		for(i=0,l=lines.length;i<l;i++) {
+
 			line = lines[i];
-			char = line.charAt(0);
 
 			tokens = tokenize(line);
 
-			console.log(tokenize2(line));
-
-			switch(char) {
-				case '/': resource(tokens); break;
-				case '#': method(tokens); break;
-				case ':': param(tokens); break;
-				case '?': query(tokens); break;
-				case '@': body(tokens); break;
-				case '$': file(tokens); break;
-				case '{': command(line); break;
-				case '.': property(tokens); break;
-				case '|': response(tokens); break;
-				default: description(line); break;
+			switch(tokens.type) {
+				case 'resource': resource(tokens); break;
+				case 'method': method(tokens); break;
+				case 'route': route(tokens); break;
+				case 'query': query(tokens); break;
+				case 'body': body(tokens); break;
+				case 'file': file(tokens); break;
+				case 'command': command(tokens); break;
+				case 'property': property(tokens); break;
+				case 'response': response(tokens); break;
+				default: description(tokens); break;
 			}
 
 		}
